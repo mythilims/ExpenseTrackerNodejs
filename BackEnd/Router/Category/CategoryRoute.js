be76abe729ba73c04b1a01efbb747046b96bfbce
mythili <mythilijobofficial@gmail.com>
2025-07-12 21:31:47 +0530

    auth validatiion added

--- BackEnd/Router/Category/CategoryRoute.js
+++ BackEnd/Router/Category/CategoryRoute.js
@@ -1,6 +1,9 @@
 const express  =require('express');
 const {getAllCategory,getCategory, createCategory, getByIdCategory, updateCategory, deleteCategory} = require('../../Controller/Category.Controller');
+const authMiddleware = require('../../Middleware/AuthMiddleware');
 const categoryRoute =express.Router();
+categoryRoute.use(authMiddleware);
+
 categoryRoute.get('/',getCategory);
 categoryRoute.get('/all',getAllCategory);
 
