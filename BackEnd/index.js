8a2ac3cd501e75091d05408c1f4b80b8d5a7bc8c
mythili <mythilijobofficial@gmail.com>
2025-07-13 10:48:24 +0530

    server

--- BackEnd/index.js
+++ BackEnd/index.js
@@ -1,35 +1,35 @@
 const express = require("express");
 const cors = require("cors");
 const dotenv = require("dotenv");
-let path =require("path");
+let path = require("path");
 const fs = require("fs");
 dotenv.config();
-const cookieParser =require('cookie-parser')
+const cookieParser = require("cookie-parser");
 const app = express();
 app.use(express.json());
-app.use(cookieParser())
+app.use(cookieParser());
 app.use(
   cors({
-    origin: "http://localhost:5173", // ✅ Must match your frontend URL
-    credentials: true                // ✅ Needed to send cookies
+    origin:
+      process.env.NODE_ENV === "production"
+        ? "https://expensetrackernodejsui.onrender.com"
+        : "http://localhost:5173", // ✅ Must match your frontend URL
+    credentials: true, // ✅ Needed to send cookies
   })
 );
-app.use(express.static('views'))
-
+app.use(express.static("views"));
 
 let PORT = process.env.PORT || 3000;
 let HOST_NAME = process.env.HOST_NAME;
 
-
 const expenseTracker = require("./Router/ExpenseTracker/ExpenseTrackerRouter.js");
 const expenseSummaryTracker = require("./Router/ExpenseTracker/ExpenseTrackerCatSummary.js");
 const authLogin = require("./Router/UserRouter/UserRouter.js");
-const  categoryRoute = require("./Router/Category/CategoryRoute.js");
-const incomeRouter =require("./Router/Income/IncomeRouter.js")
+const categoryRoute = require("./Router/Category/CategoryRoute.js");
+const incomeRouter = require("./Router/Income/IncomeRouter.js");
 
 const dbConnection = require("./DataBase/DB.js");
 dbConnection();
-
 
 app.use("/", authLogin);
 app.use("/income", incomeRouter);
@@ -37,7 +37,7 @@ app.use("/income", incomeRouter);
 app.use("/expense", expenseTracker);
 app.use("/summary", expenseSummaryTracker);
 
-app.use('/category',categoryRoute)
+app.use("/category", categoryRoute);
 
 app.use((error, req, res, next) => {
   const logDir = path.join(__dirname, "Log");
@@ -54,8 +54,10 @@ app.use((error, req, res, next) => {
       console.log("❌ Log Write Failed:", err);
     }
   });
-  
-  res.status(500).json({ message: error.message,error: "Internal Server Error" });
+
+  res
+    .status(500)
+    .json({ message: error.message, error: "Internal Server Error" });
 });
 
 app.set("views", "./Views");
@@ -63,6 +65,6 @@ app.set("view engine", "pug");
 
 app.listen(PORT, () => {
   console.log(process.env.NODE_ENV);
-  
+
   console.log(`am a server PORT:${PORT}`);
 });
